c096e7511115cc3cc858792597919b2c038687b6
Chetty <acedomarfil1a@gmail.com>
2023-10-17 09:02:48 +0200

    DNI.java CLASS ADDED

---
+++ main.java
@@ -0,0 +1,10 @@
+
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+       String sentencia_1="HolaMundo";
+        // ...
+        System.out.println(sentencia_1);
+    }
+}
+
